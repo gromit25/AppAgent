4d9131b15d5d45c3ee76e855e62ac2a665bf0eba
gromit25 <gromit25@hanmail.net>
2024-12-17 22:36:07 +0900

    주석 추가

--- src/main/java/com/redeye/appagent/logger/file/FileWriter.java
+++ src/main/java/com/redeye/appagent/logger/file/FileWriter.java
@@ -9,20 +9,28 @@ import com.redeye.appagent.Config;
 import com.redeye.appagent.logger.LogWriter;
 
 /**
- * 
+ * 파일 로그 Writer
  * 
  * @author jmsohn
  */
 public class FileWriter implements LogWriter {
 	
+	/** 로그 파일 */
 	private Path logFile;
 	
+	/**
+	 * 생성자
+	 */
 	public FileWriter() {
+		
+		// 로그 파일 객체 생성
 		this.logFile = Paths.get(Config.LOG_FILE_PATH.getValue());
 	}
 
 	@Override
 	public void write(String msg) throws Exception {
+		
+		// 메시지를 로그 파일에 write
 		Files.write(
 			this.logFile,
 			msg.getBytes(),
