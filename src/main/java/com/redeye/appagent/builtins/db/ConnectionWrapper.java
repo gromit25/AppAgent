21b360b5853be3455589a19aaeb8f2d360e3cd7e
jung mo <gromit25@hanmail.net>
2025-07-14 15:48:31 +0900

    Update and rename PreparedStatementWrapper.java to ConnectionWrapper.java

--- src/main/java/com/redeye/appagent/builtins/db/ConnectionWrapper.java
+++ src/main/java/com/redeye/appagent/builtins/db/ConnectionWrapper.java
@@ -1,13 +1,16 @@
 package com.redeye.appagent.builtins.db;
 
-@TargetClass(type = "DB", cls = "java/sql/PreparedStatement")
-public class PreparedStatementWrapper {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+@TargetClass(type = "DB", cls = "java/sql/Connection")
+public class ConnectionWrapper {
 	
 	@TargetMethod("prepareStatement(Ljava/lang/String;)Ljava/sql/PreparedStatement;")
 	public static PreparedStatement prepareStatement(Connection conn, String sql) throws SQLException {
 
+		ContentsDB.setSql(sql);
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		
 		return pstmt;
 	}
 }
