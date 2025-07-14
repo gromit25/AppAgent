e322e66100247fead0d474855b947fccdce8bbc5
jung mo <gromit25@hanmail.net>
2025-07-14 15:10:36 +0900

    Update PreparedStatementWrapper.java

--- src/main/java/com/redeye/appagent/builtins/db/PreparedStatementWrapper.java
+++ src/main/java/com/redeye/appagent/builtins/db/PreparedStatementWrapper.java
@@ -2,8 +2,12 @@ package com.redeye.appagent.builtins.db;
 
 @TargetClass(type = "DB", cls = "java/sql/PreparedStatement")
 public class PreparedStatementWrapper {
+	
 	@TargetMethod("prepareStatement(Ljava/lang/String;)Ljava/sql/PreparedStatement;")
-  public static PreparedStatement prepareStatement(String sql) throws SQLException {
-    return null;
-  }
+	public static PreparedStatement prepareStatement(Connection conn, String sql) throws SQLException {
+
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		
+		return pstmt;
+	}
 }
