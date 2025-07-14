a2f8135411810a9eec38ccb74aa46d72ce213b01
jung mo <gromit25@hanmail.net>
2025-07-14 17:12:33 +0900

    Update PreparedStatementWrapper.java

--- src/main/java/com/redeye/appagent/builtins/db/PreparedStatementWrapper.java
+++ src/main/java/com/redeye/appagent/builtins/db/PreparedStatementWrapper.java
@@ -1,6 +1,7 @@
 package com.redeye.appagent.builtins.db;
 
 import java.sql.Statement;
+import java.sql.ResultSet;
 
 @TargetClass("java/sql/Statement")
 public class PreparedStatementWrapper {
@@ -12,6 +13,19 @@ public class PreparedStatementWrapper {
 		ContentsDB.addParam(x);
 	}
 
+	@TargetMethod("execute()Z")
+	public static boolean execute(PreparedStatement pstmt) throws SQLException {
+
+		long start = System.currentTimeMillis();
+		boolean result = pstmt.execute();
+		long end = System.currentTimeMillis();
+
+		Log.write(ActionType.DB_INS.name(), pstmt, end-start, "\"sql\": \"%s\", \"params\": \"%s\"", ContentsDB.getSql(), ConstantsDB.getParams());
+		ContentsDB.clear();
+
+		return result;
+	}
+	
 	@TargetMethod("executeUpdate()I")
 	public static int executeUpdate(PreparedStatement pstmt) throws SQLException {
 
@@ -23,5 +37,18 @@ public class PreparedStatementWrapper {
 		ContentsDB.clear();
 		
 		return result;
+	}
+
+	@TargetMethod("executeQuery()Ljava/sql/ResultSet;")
+	public static ResultSet executeQuery(PreparedStatement pstmt) throws SQLException {
+
+		long start = System.currentTimeMillis();
+		ResultSet rs = pstmt.executeQuery();
+		long end = System.currentTimeMillis();
+
+		Log.write(ActionType.DB_SEL.name(), pstmt, end-start, "\"sql\": \"%s\", \"params\": \"%s\"", ContentsDB.getSql(), ConstantsDB.getParams());
+		ContentsDB.clear();
+
+		return rs;
 	}
 }
