0d2ecec2b0ce354f295d2b47b54db1d4ef892190
jung mo <gromit25@hanmail.net>
2025-07-14 16:33:54 +0900

    Update PreparedStatementWrapper.java

--- src/main/java/com/redeye/appagent/builtins/db/PreparedStatementWrapper.java
+++ src/main/java/com/redeye/appagent/builtins/db/PreparedStatementWrapper.java
@@ -2,4 +2,5 @@ package com.redeye.appagent.builtins.db;
 
 @TargetClass("java/sql/Statement")
 public class PreparedStatementWrapper {
+	@TargetMethod("executeQuery(Ljava/lang/String;)Ljava/sql/ResultSet;")
 }
