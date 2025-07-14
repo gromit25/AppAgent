0154faa8c77f0a2314986fcbd525d254ba5f6e98
jung mo <gromit25@hanmail.net>
2025-07-14 15:54:11 +0900

    Update ConnectionWrapper.java

--- src/main/java/com/redeye/appagent/builtins/db/ConnectionWrapper.java
+++ src/main/java/com/redeye/appagent/builtins/db/ConnectionWrapper.java
@@ -5,11 +5,20 @@ import java.sql.PreparedStatement;
 
 @TargetClass(type = "DB", cls = "java/sql/Connection")
 public class ConnectionWrapper {
+
+	@TargetMethod("createStatement()Ljava/sql/Statement;")
+	public static Statement createStatement() throws SQLException {
+
+		Statement stmt = conn.createStatement();
+		return stmt;
+	}
 	
 	@TargetMethod("prepareStatement(Ljava/lang/String;)Ljava/sql/PreparedStatement;")
 	public static PreparedStatement prepareStatement(Connection conn, String sql) throws SQLException {
 
 		ContentsDB.setSql(sql);
+		Log.write(ActionType.DB_SQL.name(), null, "\"sql\": \"%s\"", sql);
+		
 		PreparedStatement pstmt = conn.prepareStatement(sql);
 		return pstmt;
 	}
