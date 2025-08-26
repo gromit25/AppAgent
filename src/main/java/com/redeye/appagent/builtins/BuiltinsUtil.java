bb5dfdd7f9c3f686e73697b47d83ae1d7913ad25
jung mo <gromit25@hanmail.net>
2025-08-26 16:59:47 +0900

    Update BuiltinsUtil.java

--- src/main/java/com/redeye/appagent/builtins/BuiltinsUtil.java
+++ src/main/java/com/redeye/appagent/builtins/BuiltinsUtil.java
@@ -22,6 +22,6 @@ public class BuiltinsUtil {
     ExecuteMethod execMethod
   ) throws Exception {
     
-    measureExecutionTime(apiType, null, message, execMethod);
+    measureExecTime(apiType, null, message, execMethod);
   }
 }
