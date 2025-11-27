9a44d601bcdafd1dbaa0e94d3df1fcbf4c26073f
jung mo <gromit25@hanmail.net>
2025-11-27 10:41:14 +0900

    Update ActionType.java

--- src/main/java/com/redeye/appagent/builtins/db/ActionType.java
+++ src/main/java/com/redeye/appagent/builtins/db/ActionType.java
@@ -1,21 +1,27 @@
 package com.redeye.appagent.builtins.db;
 
 /**
- * 
+ * DB 수행 종류 코드
  * 
  * @author jmsohn
  */
 enum ActionType {
-	
+
+	/** DB 연결 */
 	DB_CON,
+	/** DB 연결 해제(close) */
 	DB_CLS,
 
+	/** sql 수행 - Merge 등등 구분이 어려운 경우 */
 	DB_SQL,
-	
+
+	/** select 수행 */
 	DB_SEL,
+	/** insert, update, delete 수행 */
 	DB_CUD,
-	
+
+	/** commit 수행 */
 	DB_CMT,
-	DB_RBK
-	;
+	/** rollback 수행 */
+	DB_RBK;
 }
